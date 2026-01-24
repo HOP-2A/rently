db32d0ceb06c1a445a16d676bb230182c4d7e79c
25hp5149 User <25hp5149@Users-iMac.local>
2026-01-24 12:19:30 +0800

    conflict fix

--- app/api/getAllBair/route.ts
+++
@@ -1,7 +0,0 @@
-import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  const listings = await prisma.listing.findMany();
-  return NextResponse.json(listings);
-}
